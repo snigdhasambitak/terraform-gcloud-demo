fe9cfb81dd26dab11b41e2216c1dbba6ad398f59
Snigdha Aryakumar <saryakumar@travix.com>
2022-03-16 21:41:39 +0100

    fix location

--- examples/main.tf
+++ examples/main.tf
@@ -1,5 +1,5 @@
-module "my_awesome_bucket" {
-  source    = "git::https://github.com/snigdhasambitak/terraform-gcloud-demo.git?ref=master"
+module "my_bucket" {
+  source    = "git::https://github.com/snigdhasambitak/terraform-gcloud-demo.git?ref=main"
   name      = "bucket"
   stage     = "production"
   namespace = "snigdha"
--- provider.tf
+++ provider.tf
@@ -3,12 +3,10 @@ terraform {
     google = {
       source  = "hashicorp/google"
       version = ">= 3.0"
-      region      = "europe-west1"
     }
     google-beta = {
       source  = "hashicorp/google"
       version = ">= 3.0"
-      region      = "europe-west1"
     }
   }
   required_version = ">= 0.14"
